4fd3bab42f456c41b316537b11db007406232821
kpkalmus <piotrek-kalmus@web.de>
2017-05-12 11:52:01 +0200

    Move calculation of charge and frequent renter points to movie.

--- src/Movie.java
+++ src/Movie.java
@@ -3,6 +3,7 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
+    
     private String title;
     private int priceCode;
     
@@ -22,5 +23,30 @@ public class Movie {
     public String getTitle (){
         return title;
     };
+ 
+    public double getCharge(int daysRented) {
+    	double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+    
+    public int getFrequentRenterPoints(int daysRented) {
+    	if((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) return 2;
+    	return 1;
+    }
     
 }
